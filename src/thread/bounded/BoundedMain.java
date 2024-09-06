fa865bd5cdff2e912e149f981bac914be4323b8c
eastshine <eastshine@pointi.com>
2024-09-06 17:48:53 +0900

    생산자, 소비자 문제 예제 3

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -1,6 +1,7 @@
 package thread.bounded;
 
 import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
@@ -10,7 +11,12 @@ public class BoundedMain {
     public static void main(String[] args) {
         // 1. BoundedQueue 선택
 //        BoundedQueueV1 queue = new BoundedQueueV1(2);
-        BoundedQueueV2 queue = new BoundedQueueV2(2);
+//        BoundedQueueV2 queue = new BoundedQueueV2(2);
+//        BoundedQueueV3 queue = new BoundedQueueV3(2);
+//        BoundedQueueV4 queue = new BoundedQueueV4(2);
+//        BoundedQueueV5 queue = new BoundedQueueV5(2);
+//        BoundedQueueV6_1 queue = new BoundedQueueV6_1(2);
+        BoundedQueueV6_2 queue = new BoundedQueueV6_2(2);
 
         // 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택!
 //        producerFirst(queue); // 생산자 먼저 실행
